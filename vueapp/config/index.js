d64a9dc7ec2117b40fb8de6d981ddac97cdad776
Phung Xuan Anh <anhpx@sigma-solutions.eu>
2018-06-04 18:15:24 +0700

    modified code

--- vueapp/config/index.js
+++ vueapp/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://localhost:8080/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
